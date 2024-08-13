1938c7f1f4f1e58196bf119d1e64db2a7ee25f74
abdalasif <52707245+abdalasif@users.noreply.github.com>
2024-08-13 14:14:46 +0500

    fix/0001 - Turn off digest authentication and use basic instead (#1)

--- src/phoneid/PhoneIdClient.php
+++ src/phoneid/PhoneIdClient.php
@@ -22,31 +22,4 @@ class PhoneIdClient extends RestClient {
   function phoneid ($phone_number, array $fields = []) {
     return $this->post(sprintf(self::PHONEID_RESOURCE, $phone_number), $fields);
   }
-  
-  protected function execute ($method_name, $resource, $fields = [], $date = null, $nonce = null) {
-    $json_fields = json_encode($fields, JSON_FORCE_OBJECT);
-    
-    $content_type = in_array($method_name, ["POST", "PUT"]) ? "application/json" : "";
-    
-    $headers = $this->generateTelesignHeaders(
-      $this->customer_id,
-      $this->api_key,
-      $method_name,
-      $resource,
-      $json_fields,
-      $date,
-      $nonce,
-      $this->user_agent,
-      $content_type
-    );
-
-    $option = in_array($method_name, [ "POST", "PUT" ]) ? "body" : "query";
-
-    return new Response($this->client->request($method_name, $resource, [
-      "headers" => $headers,
-      $option => in_array($method_name, [ "POST", "PUT"]) ? $json_fields : $fields,
-      "http_errors" => false
-    ]));
-  }
-
 }
